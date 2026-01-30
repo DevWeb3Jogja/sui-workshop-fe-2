8ec017c99a64bd5e6ae2d422935a0296de09190c
FjrREPO <jossfajar21@gmail.com>
2026-01-30 19:32:32 +0700

    chore: Update app title to "Frontend".

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,7 @@ export function App() {
       <main className="mx-2 py-32 sm:mx-8 md:mx-16 lg:mx-28 xl:mx-28">
         <Card>
           <CardHeader>
-            <CardTitle className="text-4xl font-bold">Workshop SUI - Escrow DAPP</CardTitle>
+            <CardTitle className="text-4xl font-bold">Workshop SUI - Frontend</CardTitle>
           </CardHeader>
           <CardContent className="flex flex-col gap-2">
             <span>Start Here!</span>
