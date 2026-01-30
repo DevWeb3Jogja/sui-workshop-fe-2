0616f49e41df4d9842996c64c19729b088e79b06
FjrREPO <jossfajar21@gmail.com>
2026-01-30 17:52:48 +0700

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export function App() {
+  return (
+    <body className="bg-background text-foreground bg-grid min-h-screen">
+      <main className="mx-2 py-32 sm:mx-8 md:mx-16 lg:mx-28 xl:mx-28">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-4xl font-bold">Workshop SUI - Escrow DAPP</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-2">
+            <span>Start Here!</span>
+          </CardContent>
+        </Card>
+      </main>
+    </body>
+  );
+}
